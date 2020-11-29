f671632f7835fedab88e09222f761db1c1725274
wvGithmi <wvganjana@gmail.com>
2020-11-29 23:17:16 +0530

    Added a search-bar

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ const api = {
 function App() {
   return (
     <div className="App">
-      
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+      </main> 
     </div>
   );
 }
